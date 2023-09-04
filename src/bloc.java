3ca91605ab83f663168c381fb1de666ca630b37c
Simon Destombes <simon.destombes.etu@univ-lille.fr>
2023-09-04 16:50:15 +0200

    renommage

--- src/bloc.java
+++ src/bloc.java
@@ -1,21 +1,22 @@
 package src;
 
-public class bloc {
-    public class Bloc {     
-        private int rotation;     
-        private String[][]piece;     
-        private int type;          
-        public Bloc(int type) {         
-            this.type = type;         
-            this.piece = initializeBloc(type);     
-        }     
-        public String[][] initializeBloc(int type){         
-            String[][] res = null;         
-            if(type == 1) {             
-                res = new String[][] {{"v","v","v","v"},{"v","v","v","v"},{"x","x","v","v"},{"x","x","v","v"}};         
-            }         
-            if(type == 2) {             
-                res = new String[][] {{"v","v","v","v"},{"v","v","v","v"},{"v","v","v","v"},{"x","x","x","x"}};         
-            }         return res;     
-        } }
+public class Bloc {     
+    private int rotation;     
+    private String[][]piece;     
+    private int type;
+    private Couleur couleur;          
+    public Bloc(int type) {         
+        this.type = type;         
+        this.piece = initializeBloc(type);     
+    }     
+    public String[][] initializeBloc(int type){         
+        String[][] res = null;         
+        if(type == 1) {             
+            res = new String[][] {{"v","v","v","v"},{"v","v","v","v"},{"x","x","v","v"},{"x","x","v","v"}};         
+        }         
+        if(type == 2) {             
+            res = new String[][] {{"v","v","v","v"},{"v","v","v","v"},{"v","v","v","v"},{"x","x","x","x"}};         
+        }         
+        return res;     
+    }
 }
