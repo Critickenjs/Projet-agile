7be44b5b3606453d166f4cdfe2de2df88ca9c80b
Elise MCGOVERN <elise.mcgovern.etu@epicea10.iut-infobio.priv.univ-lille1.fr>
2023-09-04 16:07:28 +0200

    plateau v1

---
+++ src/plateau.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Plateau{
+    private final int LARGEUR = 1;
+    private final int TAILLE = 10;
+    private Objet plateau[][]; 
+
+    public Plateau(){
+        plateau = new Objet[LARGEUR][TAILLE];
+    }
+
+    
+}
