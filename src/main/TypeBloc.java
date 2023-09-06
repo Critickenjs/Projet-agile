5942ff6861e54ecd46c09e0722d3d5fabcdde07c
noé decobecq <noedecobecq@gmail.com>
2023-09-06 14:49:06 +0200

    type bloc rajout des blocs supprimés

--- src/main/TypeBloc.java
+++ src/main/TypeBloc.java
@@ -1,12 +1,11 @@
 package main;
-import java.util.ArrayList;
-import java.util.List;
 
 public enum TypeBloc {
 	
 	Carre(new int[][]{{3,0},{3,1},{2,0},{2,1}},Couleur.BLUE),Ligne(new int[][]{{3,0},{3,1},{3,2},{2,3}},Couleur.GREEN),
 	Zig(new int[][]{{3,0},{3,1},{2,1},{2,2}},Couleur.RED),Zag(new int[][]{{3,0},{2,0},{2,1},{1,1}},Couleur.YELLOW),
-	T(new int[][]{{3,0},{3,1},{3,2},{2,1}},Couleur.PURPLE),Coin1(new int[][]{{3,0},{3,1},{2,1},{1,1}},Couleur.ORANGE);
+	T(new int[][]{{3,0},{3,1},{3,2},{2,1}},Couleur.PURPLE),Coin1(new int[][]{{3,0},{3,1},{2,1},{1,1}},Couleur.ORANGE),
+	Coin2(new int[][]{{3,0},{3,1},{2,1},{1,1}},Couleur.CYAN),Ligne2(new int[][]{{3,0},{2,0},{1,0},{0,0}},Couleur.GREEN);
 	
 	int[][]coordonnees;
 	Couleur couleur;
